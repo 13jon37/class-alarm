f019ed92835e1973a55bd51a1e3f886292258006
Jon <jon@iMac.local>
2020-08-19 12:41:54 -0400

    small changes

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import time, gtts, os, pafy, vlc
 from time import strftime
 from playsound import playsound
 
-TIME_CLASS = "9:30"
+TIME_CLASS = "12:32"
 
 def play_sound():
 	playsound("class.mp3") # Play mp3 file already downloaded
 	time.sleep(3.5)
 
 def music():
-	url = "https://www.youtube.com/watch?v=s8qWpCMgecs"
+	url = "https://www.youtube.com/watch?v=4WkcYaAecOQ"
 	video = pafy.new(url)
 	best = video.getbest()
 	playurl = best.url
@@ -27,14 +27,16 @@ def main():
 # If local time = class time playvoice
 	while True:
 		HM = strftime("%H:%M", time.localtime())
+		local_time = time.asctime(time.localtime())
 		# Get local time and convert to string
 		if(str(HM) == TIME_CLASS):
-			print(f"Time for class! {HM}")
+			print(f"Time for class! {HM}\n")
+			print(f"{local_time}")
 			play_sound()
 			music()
 			os.system("clear")
 		else:
-			print(f"Not time for class. {HM}")
+			print(f"{local_time}")
 			os.system("clear")
 	time.sleep(1)
 
