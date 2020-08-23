1657e9f71b0a9a42fbb725fea9df05bcc59e349b
Jon <jon@iMac.fios-router.home>
2020-08-23 12:27:36 -0400

    small changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time, os
+import time, os, pyfiglet
 
 from time import strftime
 
@@ -10,35 +10,57 @@ from voice import Voice
 # Program will play voice saying "time for class" and a video
 # at the designated time
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 class Alarm:
 
 	def __init__(self):
-		self.AWAKE_TIME = "10:48"
-		self.CLASS_TIME = "11:35"
-		self.music = Music()
+		self.ascii_banner = pyfiglet.figlet_format("Hello!!")
+		self.ascii_banner2 = pyfiglet.figlet_format("1337 Alarm")
+		self.AWAKE_TIME = "06:30"
+		self.CLASS_TIME = "09:25"
+		self.music = Music() 
 		self.voice = Voice()
 		
 	def run(self):
+		os.system('resize -s 12 55') # Resize terminal window on mac
+
+		# Print Hello in ascii art in blue color
+		print(f"{bcolors.OKBLUE}{self.ascii_banner}")
+		time.sleep(3)
 		while True:
+			# Print "1336 Alarm" ascii banner in blue
+			print(f"{bcolors.OKBLUE}{self.ascii_banner2}")
+
+			# Get & store H&M of local time as a string
 			current_time = strftime("%H:%M", time.localtime())
+			# Get and store local time fully as in Day, Mnth, Hours, mins, etc.
 			full_time = time.asctime(time.localtime())
 
 			if current_time == self.AWAKE_TIME:
-				print(f"Time to wake up! {current_time}\n")
-				print(f"{full_time}")
+				print(f"Time to wake up! {bcolors.WARNING}{current_time}\n")
+				print(f"{bcolors.OKGREEN}{full_time}")
 
 				# Call voice & music functions
 				self.voice.play_voice_awake()
-				time.sleep(3000)
 				self.music.play_music()
 			elif current_time == self.CLASS_TIME:
-				print(f"Time for class! {current_time}\n")
+				print(f"Time for class!{bcolors.WARNING}{current_time}\n")
 				print(f"{full_time}")
 
 				self.voice.play_voice_class()
 			else:
-				print(f"{full_time}")
-			os.system("clear")		# ('cls') for windwows
+				print(f"{bcolors.OKGREEN}{full_time}")
+			time.sleep(1) # To stop console flickering
+			os.system("clear") # Clear the console ('cls') for windwows
 
 
 
